16b72bad4d754d254c8a5e8b01e0516059d99593
Taha-Saber <tahasaberts1@gmail.com>
2026-03-10 22:02:55 +0200

    SVU-51 add splash screen2

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:lms_student/features/home/data/model/course_model.dart';
 
 abstract class HomeRepository {
-  // الأساسيات اللي محتاجها أكيد
   Future<Either<List<CourseModel>, String>> getAllCourses();
   Future<Either<CourseModel, String>> getCourseById(String slug);
   Future<Either<List<CourseModel>, String>> searchInCourses(String query);
